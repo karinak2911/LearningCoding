e13bf9808dc8cb08014a0c82c77955b64a6dfcd8
Navi <Navi@karinas-mbp>
2021-08-31 19:31:08 +0200

    Math random, downlad file and check

---
+++ src/main/java/arrayOfObjects/StockArray.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayOfObjects;
+
+/**
+ *
+ * @author Navi
+ */
+public class StockArray {
+    private StockItem [] arr = new StockItem[150]; 
+    private int size; 
+    
+    
+    
+}
---
+++ src/main/java/arrayOfObjects/StockItem.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayOfObjects;
+
+/**
+ *
+ * @author Navi
+ */
+public class StockItem {
+    private String description; 
+    private int costPrice; 
+    private int numberInStock; 
+    private static double markUp = 0.25; 
+
+    public StockItem(String description, int costPrice, int numberInStock) {
+        this.description = description;
+        this.costPrice = costPrice;
+        this.numberInStock = numberInStock;
+    }
+    
+    private String stockCode = this.description.substring(0, 3) +  String.valueOf(Math.random() * 10).substring(0) + String.valueOf(Math.random() * 10).substring(0) + String.valueOf(Math.random() * 10).substring(0); 
+    
+    public String getStockCode(){ 
+        return stockCode; 
+    }
+    
+    
+    public double getSellingPrice(){  
+       double markUpOnItem = this.costPrice * markUp;
+       double sellingPrice = markUpOnItem + this.costPrice; 
+       return sellingPrice; 
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(int costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public int getNumberInStock() {
+        return numberInStock;
+    }
+
+    public void setNumberInStock(int numberInStock) {
+        this.numberInStock = numberInStock;
+    }
+    
+    
+}
